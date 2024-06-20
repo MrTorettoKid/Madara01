8adece2f8909cbb43594d28689b2130d7a24796a
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-06-21 00:40:15 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -154,39 +154,72 @@ REPLY_ERROR = """<code>Use this command as a reply to any telegram message with 
 
 # =====================================================================================##
 
-
 @Bot.on_message(filters.command('start') & filters.private)
-async def not_joined(client: Client, message: Message):
-    buttons = [
-        [
-            InlineKeyboardButton(text="Join Channel", url=client.invitelink),
-            InlineKeyboardButton(text="Join Channel", url=client.invitelink2),
-        ]
-    ]
-    try:
-        buttons.append(
+async def not_joined(client: Client , message: ChatJoinRequest):
+    if id in check_id:
+        buttons = [
             [
-                InlineKeyboardButton(
-                    text = 'Try Again',
-                    url = f"https://t.me/{client.username}?start={message.command[1]}"
+                InlineKeyboardButton(text="Join Channel" , url=client.invitelink) ,
+                InlineKeyboardButton(text="Join Channel" , url=client.invitelink2) ,
+            ]
+        ]
+        try:
+            buttons.append(
+                [
+                    InlineKeyboardButton(
+                        text='Try Again' ,
+                        url=f"https://t.me/ai_test_pic_bot?start=start"
+                    )
+                ]
+            )
+        except IndexError:
+            pass
+
+        await message.reply(
+            text=FORCE_MSG.format(
+                first=message.from_user.first_name ,
+                last=message.from_user.last_name ,
+                username=None if not message.from_user.username else '@' + message.from_user.username ,
+                mention=message.from_user.mention ,
+                id=message.from_user.id
+            ) ,
+            reply_markup=InlineKeyboardMarkup(buttons) ,
+            quote=True ,
+            disable_web_page_preview=True
+        )
+    elif id not in check_id:
+        buttons = [
+            [
+                InlineKeyboardButton(text="Join Channel" , url=client.invitelink) ,
+                InlineKeyboardButton(text="Join Channel" , url=client.invitelink2) ,
+                InlineKeyboardButton("JOIN CHANNEL" , url=RE_FORCE_SUB_CHANNEL
                 )
             ]
+        ]
+        try:
+            buttons.append(
+              [
+                InlineKeyboardButton(
+                    text='Try Again' ,
+                    url=f"https://t.me/{client.username}?start={message.command[1]}"
+                )
+              ]
+            )
+        except IndexError:
+           pass
+        await message.reply(
+            text=FORCE_MSG.format(
+                first=message.from_user.first_name ,
+                last=message.from_user.last_name ,
+                username=None if not message.from_user.username else '@' + message.from_user.username ,
+                mention=message.from_user.mention ,
+                id=message.from_user.id
+            ) ,
+            reply_markup=InlineKeyboardMarkup(buttons) ,
+            quote=True ,
+            disable_web_page_preview=True
         )
-    except IndexError:
-        pass
-
-    await message.reply(
-        text = FORCE_MSG.format(
-                first = message.from_user.first_name,
-                last = message.from_user.last_name,
-                username = None if not message.from_user.username else '@' + message.from_user.username,
-                mention = message.from_user.mention,
-                id = message.from_user.id
-            ),
-        reply_markup = InlineKeyboardMarkup(buttons),
-        quote = True,
-        disable_web_page_preview = True
-    )
+        print(check_id.append(id))
 
 @Bot.on_message(filters.command('users') & filters.private & filters.user(ADMINS))
 async def get_users(client: Bot, message: Message):
