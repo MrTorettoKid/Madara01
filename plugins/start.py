80ccb5e134a57b86b241699eb970db642dd673dc
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-06-21 00:17:03 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,5 +1,6 @@
 import os
 import asyncio
+from pyrogram.types import *
 from pyrogram import Client, filters, __version__
 from pyrogram.enums import ParseMode
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
